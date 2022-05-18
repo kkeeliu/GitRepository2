ab75dd96a03cb6e61fb1241bcc514839201fe374
liuke <liukechn@163.com>
2022-05-18 09:13:36 +0800

    新建了一个Student类，添加了main方法

---
+++ TestModule1/src/com/liu/www/Student.java
@@ -0,0 +1,13 @@
+package com.liu.www;
+
+/**
+ * Studen类
+ *
+ * @author : liuke
+ * @date : 2022-05-18 09:11
+ **/
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("Hello World！");
+    }
+}
